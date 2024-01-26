abeb46ac2eb8d3d81f27c9f5c011c0314d5c8c18
AnastasiiaPimkin <pimkinaanastasiia@gmail.com>
2024-01-26 11:52:58 +0200

    Update 8.2 slices.go
    upd getMessageWithRetriesForPlan method

--- ch8/8.2 slices.go
+++ ch8/8.2 slices.go
@@ -1,25 +1,46 @@
 package main
 
-import "fmt"
-
-const (
-	retry1 = "click here to sign up"
-	retry2 = "pretty please click here"
-	retry3 = "we beg you to sign up"
+import (
+	"errors"
+	"fmt"
 )
 
-func getMessageWithRetries() [3]string {
-	//messages := [3]string {retry1, retry2, retry3}
-	return [3]string{retry1, retry2, retry3}
+const (
+	planFree = "free"
+	planPro  = "pro"
+)
+
+func getMessageWithRetriesForPlan(plan string) ([]string, error) {
+	allMessages := getMessageWithRetries()
+	if plan == planFree {
+		return allMessages[0:2], nil
+	} else if plan == planPro {
+		return allMessages[:], nil
+	} else {
+		return []string{}, errors.New("unsupported plan")
+	}
 }
 
 // don't touch below this line
 
-func testSend(name string, doneAt int) {
+func getMessageWithRetries() [3]string {
+	return [3]string{
+		"click here to sign up",
+		"pretty please click here",
+		"we beg you to sign up",
+	}
+}
+
+func test(name string, doneAt int, plan string) {
+	defer fmt.Println("=====================================")
 	fmt.Printf("sending to %v...", name)
 	fmt.Println()
 
-	messages := getMessageWithRetries()
+	messages, err := getMessageWithRetriesForPlan(plan)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 	for i := 0; i < len(messages); i++ {
 		msg := messages[i]
 		fmt.Printf(`sending: "%v"`, msg)
@@ -29,14 +50,14 @@ func testSend(name string, doneAt int) {
 			break
 		}
 		if i == len(messages)-1 {
-			fmt.Println("complete failure")
+			fmt.Println("no response")
 		}
 	}
 }
 
 func main() {
-	testSend("Bob", 0)
-	testSend("Alice", 1)
-	testSend("Mangalam", 2)
-	testSend("Ozgur", 3)
+	test("Ozgur", 3, planFree)
+	test("Jeff", 3, planPro)
+	test("Sally", 2, planPro)
+	test("Sally", 3, "no plan")
 }
